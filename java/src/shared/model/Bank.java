3a3a84c9d2c44f903baae40d8cbaea07b05f7d9a
Matthew Carlson <matthewfcarlson@byu.net>
2016-01-20 10:54:30 -0700

    Finished Javadoc for bank

--- java/src/shared/model/Bank.java
+++ java/src/shared/model/Bank.java
@@ -37,42 +37,92 @@ public class Bank
 			return false;
 		}
 	}
+	/**
+	 * Attempts to get a piece (road, settlement, city)
+	 * @param type it can be anything but the robber
+	 * @return true or false depending on success
+	 */
 	public boolean getPiece(PieceType type)
 	{
+		if (type == PieceType.ROBBER) return false;
 		return false;
 	}
+	/**
+	 * This gets a dev card which by default are shuffled when given. 
+	 * @return the type of the card you received or UNKNOWN if there are no more cards left
+	 */
 	public DevCardType getDevCard()
 	{
 		return DevCardType.UNKNOWN;
 	}
+	
+	/**
+	 * Gives the bank one resource of type
+	 * @param type the resource given
+	 */
 	public void giveResource(ResourceType type)
 	{
 		
 	}
+	
+	/**
+	 * Gives the bank one piece
+	 * @param type
+	 */
 	public void givePiece(PieceType type)
 	{
 		
 	}
+	
+	/**
+	 * Gives the bank one dev card- it will be shuffled into pile
+	 * @param type
+	 */
 	public void giveDevCard(DevCardType type)
 	{
 		
 	}
+	
+	/**
+	 * Gets the current number of all held resources
+	 * @return the number of resources
+	 */
 	public int getResourceCount()
 	{
 		return 0;
 	}
+	
+	/**
+	 * returns the number of resources for a specified type
+	 * @param type
+	 * @return
+	 */
 	public int getResourceCount(ResourceType type)
 	{
 		return 0;
 	}
+	
+	/**
+	 * Gets the number of pieces available for a type held
+	 * @param type
+	 * @return
+	 */
 	public int getPiecesCount(PieceType type)
 	{
 		return 0;
 	}
+	
+	/**
+	 * Gets the number of dev cards of a type held
+	 * @param type
+	 * @return
+	 */
 	public int getDevCardCount(DevCardType type)
 	{
 		return 0;
 	}
+	
+	
 	/* (non-Javadoc)
 	 * @see java.lang.Object#hashCode()
 	 */
