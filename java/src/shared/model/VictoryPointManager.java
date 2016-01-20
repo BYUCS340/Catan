ef8e29da5b5dcb9ff8d3fbd735222db48d06264c
Garrett Egan <egan.garrett@gmail.com>
2016-01-19 22:42:12 -0700

    Stubbed VictoryPointManager class

---
+++ java/src/shared/model/VictoryPointManager.java
@@ -0,0 +1,11 @@
+package shared.model;
+
+public class VictoryPointManager
+{
+	//This class keeps track of each players' victory points.
+	//This way we keep points centralized and they're easier to track/change (especially longest road & largest army).
+	public int getVictoryPoints(int playerIndex)
+	{
+		return 0;
+	}
+}
