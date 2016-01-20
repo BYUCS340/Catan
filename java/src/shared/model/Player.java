853ff9267f43fdc3387cb75d6ad3d627f79411e2
Matthew Carlson <matthewfcarlson@byu.net>
2016-01-20 15:17:47 -0700

    added more java docs

--- java/src/shared/model/Player.java
+++ java/src/shared/model/Player.java
@@ -1,13 +1,26 @@
 package shared.model;
 
 import shared.definitions.CatanColor;
-
+/**
+ * A player in the game. Can be an AI player or a human player
+ * @author matthewcarlson, garrettegan
+ *
+ */
 public class Player
 {
 	public String name;
 	public CatanColor color;
 	public Bank playerBank;
 	private int playerIndex;
+	private boolean isRobot = false;
+	
+	public Player(String name, int index, CatanColor playerColor, boolean isHuman){
+		this.isRobot = !isHuman;
+		this.name = name;
+		this.playerBank = new Bank();
+		this.playerIndex = index;
+		this.color = playerColor;
+	}
 	
 	public boolean buildRoad()
 	{
@@ -20,5 +33,22 @@ public class Player
 	public boolean buildCity()
 	{
 		return false;
+	}
+	
+	/**
+	 * A simple function to determine if the player is controlled by AI
+	 * @return true if is a robot
+	 */
+	public boolean isARobot(){
+		return isRobot;
+	}
+	
+	/**
+	 * Returns the playerIndex so it' can't be changed 
+	 * @return 0 to 3
+	 */
+	public int playerIndex(){
+		return playerIndex;
+		
 	}
 }
--- java/src/shared/model/VictoryPointManager.java
+++ java/src/shared/model/VictoryPointManager.java
@@ -1,11 +1,62 @@
 package shared.model;
-
+/**
+ * An object that keeps track of how many victory points each player has. 
+ * This way we keep points centralized and they're easier to track/change (especially longest road & largest army).
+ * @author matthewcarlson, garrettegan
+ *
+ */
 public class VictoryPointManager
 {
+	private int currentLongestRoadPlayer = -1;
+	private int currentLargestArmyPlayer = -1;
+	private int[] victoryPoints = new int[4];
+	
+	
 	//This class keeps track of each players' victory points.
-	//This way we keep points centralized and they're easier to track/change (especially longest road & largest army).
+	
+	/**
+	 * Gets the current number of victory points the current player has
+	 * @param playerIndex 0 to 3
+	 * @return 0-10
+	 */
 	public int getVictoryPoints(int playerIndex)
 	{
 		return 0;
+	}
+	
+	/**
+	 * Tells the victory point manager which player now has the longest road- determined by map?
+	 * @param playerIndex 0 to 3
+	 * @return if successful (almost always does)
+	 */
+	public boolean setPlayerToHaveLongestRoad(int playerIndex)
+	{
+		if (playerIndex < 0 || playerIndex > 3) return false;
+		
+		return true;
+	}
+	
+	/**
+	 * Tells the VP manager which player now has the largest army - determined by @see map?
+	 * @param playerIndex 0 to 3
+	 * @see Map
+	 * @return successful or not (almost always is true
+	 */
+	public boolean setPlayerToHaveLargestArmy(int playerIndex){
+		if (playerIndex < 0 || playerIndex > 3) return false;
+		
+		return true;
+	}
+	
+	/**
+	 * Gets the specified player's current number of victory points
+	 * @param playerIndex
+	 * @see Map
+	 * @return an int between 0 and 10, -1 if invalid index
+	 */
+	public int currentPlayerScore(int playerIndex){
+		if (playerIndex < 0 || playerIndex > 3) 
+			return -1;
+		return victoryPoints[playerIndex];
 	}
 }
