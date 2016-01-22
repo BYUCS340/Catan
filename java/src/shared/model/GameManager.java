2f8ad507875cc336a7b61cf385de3fc7d005566e
Garrett Egan <egan.garrett@gmail.com>
2016-01-22 10:47:17 -0700

    Building Functions and JavaDocs
    Wrote functions for building roads, settlements, cities, and devCards in
    bank class, including canDo methods.  These only check if the bank has
    enough cards, not if the player is allowed to place a city on the map.
    Also added some JavaDocs for the game model.

--- java/src/shared/model/GameManager.java
+++ java/src/shared/model/GameManager.java
@@ -2,12 +2,20 @@ package shared.model;
 
 import java.util.List;
 
+import client.map.MapController;
+
+/**
+ * The game manager class acts as a facade between the player/game objects and the ServerProxy/UI
+ * @author matthewcarlson, garrettegan
+ *
+ */
 public class GameManager
 {
 	private GameState gameState;
 	private Bank gameBank;
 	private List<Player> players;
 	private VictoryPointManager victoryPointManager;
+	private MapController mapController;
 	
 	/**
 	 * 
