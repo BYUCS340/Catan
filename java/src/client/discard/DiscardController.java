17f3f123a6ef8d87ae8cb56cb819c44b38654796
eaglewyng <parker.ridd@byu.net>
2016-02-20 11:57:43 -0700

    Added functionality to DiscardController. The button now enables and disables, and the up and down buttons now disappear when you can't add or remove resources, respectively.

--- java/src/client/discard/DiscardController.java
+++ java/src/client/discard/DiscardController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import client.base.Controller;
 import client.misc.IWaitView;
 import client.model.ClientGame;
+import client.model.ClientGameManager;
+import shared.definitions.GameRound;
+import shared.definitions.ModelNotification;
 import shared.definitions.ResourceType;
 import shared.model.ModelObserver;
 
@@ -28,6 +31,7 @@ public class DiscardController extends Controller implements IDiscardController,
 		super(view);
 		this.waitView = waitView;
 		this.initResourceList();
+		ClientGame.getGame().startListening(this, ModelNotification.STATE);
 	}
 
 	public IDiscardView getDiscardView() {
@@ -40,53 +44,118 @@ public class DiscardController extends Controller implements IDiscardController,
 
 	@Override
 	public void increaseAmount(ResourceType resource) {
+		IDiscardView dView = getDiscardView();
+		int resourceIdx = -1;
 		switch(resource)
 		{
 			case BRICK:
 				resourceList.set(0, resourceList.get(0) + 1);
+				resourceIdx = 0;
+				break;
 			case ORE:
 				resourceList.set(1, resourceList.get(1) + 1);
+				resourceIdx = 1;
+				break;
 			case SHEEP:
 				resourceList.set(2, resourceList.get(2) + 1);
+				resourceIdx = 2;
+				break;
 			case WHEAT:
 				resourceList.set(3, resourceList.get(3) + 1);
+				resourceIdx = 3;
+				break;
 			case WOOD:
 				resourceList.set(4, resourceList.get(4) + 1);
-			default:
+				resourceIdx = 4;
 				break;
+			default:
+				return;
 		}
+		
+		int maxAmt = ClientGame.getGame().playerResourceCount(resource);
+		int currAmt = resourceList.get(resourceIdx);
+		boolean increase = currAmt < maxAmt;
+		
+		dView.setResourceDiscardAmount(resource, currAmt);
+		dView.setResourceAmountChangeEnabled(resource, increase, true);
+		updateDiscardStatus();
 	}
 
 	@Override
 	public void decreaseAmount(ResourceType resource) {
+		IDiscardView dView = getDiscardView();
+		int resourceIdx = -1;
 		switch(resource)
 		{
 			case BRICK:
 				resourceList.set(0, resourceList.get(0) - 1);
+				resourceIdx = 0;
+				break;
 			case ORE:
 				resourceList.set(1, resourceList.get(1) - 1);
+				resourceIdx = 1;
+				break;
 			case SHEEP:
 				resourceList.set(2, resourceList.get(2) - 1);
+				resourceIdx = 2;
+				break;
 			case WHEAT:
 				resourceList.set(3, resourceList.get(3) - 1);
+				resourceIdx = 3;
+				break;
 			case WOOD:
 				resourceList.set(4, resourceList.get(4) - 1);
-			default:
+				resourceIdx = 4;
 				break;
-		}		
+			default:
+				return;
+		}	
+		
+		int maxAmt = ClientGame.getGame().playerResourceCount(resource);
+		int currAmt = resourceList.get(resourceIdx);
+		boolean decrease = currAmt > 0;		
+		boolean increase = currAmt < maxAmt;
+
+		getDiscardView().setResourceDiscardAmount(resource, resourceList.get(resourceIdx));
+		dView.setResourceAmountChangeEnabled(resource, increase, decrease);
+		updateDiscardStatus();
+	}
+	
+	private void updateDiscardStatus()
+	{
+		int total = 0;
+		for(Integer i : resourceList)
+		{
+			total += i;
+		}
+		
+		int discardAmt = (ClientGame.getGame().playerResourceCount(ResourceType.BRICK) + 
+				ClientGame.getGame().playerResourceCount(ResourceType.ORE) + 
+				ClientGame.getGame().playerResourceCount(ResourceType.SHEEP) + 
+				ClientGame.getGame().playerResourceCount(ResourceType.WHEAT) + 
+				ClientGame.getGame().playerResourceCount(ResourceType.WOOD)) / 2;
+		
+		getDiscardView().setStateMessage("" + total + "/" + discardAmt);
+		getDiscardView().setDiscardButtonEnabled(total == discardAmt);
+		
 	}
 
 	@Override
 	public void discard() {
 		ClientGame.getGame().DiscardCards(resourceList);
 		getDiscardView().closeModal();
+		initResourceList();
 		
 	}
 
 	@Override
 	public void alert()
 	{
-		// TODO Auto-generated method stub
+		ClientGameManager game = ClientGame.getGame();
+		if(game.CurrentState() == GameRound.ROBBING)
+		{
+			
+		}
 		
 	}
 	
