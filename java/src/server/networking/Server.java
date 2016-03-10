9958ec2cc82e68e5f438aa9419e604bdee55e7f1
eaglewyng <parker.ridd@byu.net>
2016-03-10 14:36:01 -0700

    Stubbed out server main method

---
+++ java/src/server/networking/Server.java
@@ -0,0 +1,40 @@
+package server.networking;
+
+public class Server
+{
+	
+	private Server()
+	{
+		//TODO fill in
+	}
+	
+	private Server(int port)
+	{
+		//TODO fill in
+	}
+	
+	/**
+	 * Runs the server
+	 */
+	private void run()
+	{
+		/**
+		 * TODO
+		 * 1. Initialize models and tables
+		 * 2. Create contexts with handlers
+		 * 3. Start the server
+		 */
+	}
+	
+	/**
+	 * Runs the server
+	 * @param args Accepts 1 argument: the port to run the server on
+	 * If not specified or valid, server will be run on port 8081
+	 */
+	private static void main(String[] args)
+	{
+		//TODO fill in
+	}
+	
+	
+}
