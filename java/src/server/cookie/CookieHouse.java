7e382e174cda4945c6d384254624b299429f4225
Matthew Carlson <matthewfcarlson@byu.net>
2016-03-10 19:54:20 -0700

    More cookie house

--- java/src/server/cookie/CookieHouse.java
+++ java/src/server/cookie/CookieHouse.java
@@ -1,5 +1,10 @@
 package server.cookie;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import server.model.GameException;
+
 /**
  * Keeps track of a user's activity 
  * @author matthewcarlson
@@ -7,8 +12,33 @@ package server.cookie;
  */
 public class CookieHouse {
 
+	private Map<String,ServerCookie> cookies;
+	
+	public CookieHouse()
+	{
+		cookies = new HashMap<>();
+	}
+	
+	/**
+	 * creates a new cookie for the player ID
+	 * @param playerID
+	 * @return
+	 */
 	public ServerCookie bakeCookie(int playerID)
 	{
+		return null;
 		
+	}
+	
+	/**
+	 * Gets the cookie for the cookie text
+	 * @param cookieText
+	 * @return
+	 * @throws GameException 
+	 */
+	public ServerCookie checkCookie(String cookieText) throws GameException 
+	{
+		//TODO either create new exception type or return null
+		throw new GameException("This cookie is bad");
 	}
 }
--- java/src/server/cookie/ServerCookie.java
+++ java/src/server/cookie/ServerCookie.java
@@ -7,16 +7,41 @@ public class ServerCookie implements Cookie
 	private long cookieID = 0;
 	private int  playerID = 0;
 	private int  gameID = 0;
+	private long created;
+	private final long minutesValid = 180;
 	
-	public ServerCookie()
+	public ServerCookie(int playerid)
 	{
-		
-		
+		this.playerID = playerid;
+		created = System.currentTimeMillis();
+		cookieID = (long) (Math.random() * Long.MAX_VALUE);
 	}
+	
 	@Override
-	public String getCookieText() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getCookieText()
+	{
+		return String.valueOf(cookieID);
+	}
+	
+	public int getPlayerID()
+	{
+		return this.playerID;
+	}
+	
+	public boolean equals(String cookieText)
+	{
+		return this.getCookieText().equals(cookieText);
+	}
+	
+	/**
+	 * Checks if the cookie has expired (180 minutes)
+	 * @return true or false
+	 */
+	public boolean isExpired()
+	{
+		long currentTime = System.currentTimeMillis();
+		long expiredTime = this.created + (minutesValid * 1000 * 60);
+		return currentTime  < expiredTime;
 	}
 
 }
