eca62b17282059d7d265b33f50d7e2181241d3b4
Matthew Carlson <matthewfcarlson@byu.net>
2016-03-14 16:05:12 -0600

    Added fix to make travis happy.

--- java/src/server/model/GameTable.java
+++ java/src/server/model/GameTable.java
@@ -30,7 +30,6 @@ public class GameTable
 		games = new HashMap<>();
 		cookieTreeHouse = new CookieHouse();
 		playerTable = new PlayerDen();
-		this.CreateGame("Default Game", false, false, false);
 	}
 	/**
 	 * Creates a new game on the server 
