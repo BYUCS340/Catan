09fa3a93b2a3fd592e3f2b7e2fa98b7bee7e2e26
Jonathan Sadler <jesdlr@live.com>
2016-03-14 16:22:24 -0600

    Create log class for handling the log

--- java/src/server/Server.java
+++ java/src/server/Server.java
@@ -4,9 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.logging.*;
 
 import com.sun.net.httpserver.*;
@@ -18,12 +15,7 @@ public class Server
 	private static final int DEFAULT_PORT = 8081;
 	private static final int MAX_WAITING = 20;
 	
-	private static final Level DEFAULT_CONSOLE_LEVEL = Level.FINEST;
-	private static final String DEFAULT_LOG_FILE = "CatanServer.log";
-	
 	private static String SwaggerPath = "";
-	
-	private Logger logger;
 	
 	public static void main(final String[] args) 
 	{
@@ -70,14 +62,12 @@ public class Server
 	}
 	
 	private void Initialize(int port)
-	{
-		InitializeLogger();
-		
+	{	
 		try 
 		{
 			Level defaultLevel = Level.FINE;
 			
-			logger.log(defaultLevel, "Starting server");
+			Log.GetLog().log(defaultLevel, "Starting server");
 			HttpServer server = HttpServer.create(new InetSocketAddress(port), MAX_WAITING);
 			
 			server.createContext("/", new HTTPHandler());
@@ -87,61 +77,14 @@ public class Server
 			
 			server.start();
 			
-			logger.log(defaultLevel, "Server created successfully");
-			logger.log(defaultLevel, "IP: " + InetAddress.getLocalHost().getHostAddress());
-			logger.log(defaultLevel, "Port: " + port);
+			Log.GetLog().log(defaultLevel, "Server created successfully");
+			Log.GetLog().log(defaultLevel, "IP: " + InetAddress.getLocalHost().getHostAddress());
+			Log.GetLog().log(defaultLevel, "Port: " + port);
 		}
 		catch (IOException e) 
 		{
-			logger.log(Level.SEVERE, e.getMessage(), e);
+			Log.GetLog().log(Level.SEVERE, e.getMessage(), e);
 			return;
-		}
-	}
-	
-	private void InitializeLogger()
-	{
-		logger = Logger.getLogger("CatanServer");
-		logger.setLevel(Level.FINEST);
-		logger.setUseParentHandlers(false);
-		
-		ConsoleHandler consoleHandler = new ConsoleHandler();
-		consoleHandler.setLevel(DEFAULT_CONSOLE_LEVEL);
-		consoleHandler.setFormatter(new logFormatter());
-		logger.addHandler(consoleHandler);
-		
-		try
-		{
-			FileHandler fileHandler = new FileHandler(DEFAULT_LOG_FILE, false);
-			fileHandler.setLevel(Level.FINEST);
-			fileHandler.setFormatter(new logFormatter());
-			logger.addHandler(fileHandler);
-		}
-		catch (IOException ex)
-		{
-			logger.warning("Unable to attach file handler");
-		}
-		
-		logger.log(Level.FINE, "Logger initialized");
-	}
-	
-	private static class logFormatter extends Formatter
-	{
-		@Override
-		public String format(LogRecord record)
-		{
-			Date date = new Date(record.getMillis());
-			DateFormat formatter = new SimpleDateFormat("hh:mm:ss.SSS");
-			String dateFormatted = formatter.format(date);
-			
-			if (record.getThrown() == null)
-			{
-				return dateFormatted + "-" + record.getMessage() + "\n";
-			}
-			else
-			{
-				return dateFormatted + "-EXCEPTION!\n--Class=" + record.getSourceClassName() 
-					+ "\n--Method=" + record.getSourceMethodName() + "\n--Message=" + record.getThrown().getMessage() + "\n";
-			}
 		}
 	}
 }
