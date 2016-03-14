454586ef9478e721e037c944ceccb0341b1716fe
Matthew Carlson <matthewfcarlson@byu.net>
2016-03-14 15:52:55 -0600

    Added game list stuff

--- java/src/server/commands/games/GamesListCommand.java
+++ java/src/server/commands/games/GamesListCommand.java
@@ -1,6 +1,14 @@
 package server.commands.games;
 
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
 import server.commands.ICommand;
+import server.model.GameArcade;
+import shared.data.GameInfo;
+import shared.networking.GSONUtils;
 
 /**
  * Command to get all the games from the server.
@@ -12,16 +20,18 @@ public class GamesListCommand implements ICommand
 	/**
 	 * Creates a command to get a list of games.
 	 */
+	private List<GameInfo> games;
 	public GamesListCommand()
 	{
-		return;
+		
 	}
 
 	@Override
 	public boolean Execute() 
 	{
 		// TODO Auto-generated method stub
-		return false;
+		games = GameArcade.games().GetAllGames();
+		return true;
 	}
 
 	@Override
@@ -33,8 +43,10 @@ public class GamesListCommand implements ICommand
 	@Override
 	public String Response() 
 	{
-		// TODO Auto-generated method stub
-		return null;
+		Iterator<GameInfo> iter = games.iterator();
+		while (iter.hasNext()) System.out.println(iter.next());
+		System.out.println(games);
+		return GSONUtils.serialize(games);
 	}
 
 }
