7be998d0e074f8ac776ef79951def8976f2fe591
Matthew Carlson <matthewfcarlson@byu.net>
2016-03-15 22:16:11 -0600

    Started to implement methods on server game manager based on what the swagger wants

--- java/src/server/model/ServerGameManager.java
+++ java/src/server/model/ServerGameManager.java
@@ -1,7 +1,12 @@
 package server.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import shared.definitions.CatanColor;
 import shared.model.GameManager;
 import shared.model.GameModel;
+import shared.model.ModelException;
 import shared.model.map.model.MapGenerator;
 import shared.model.map.model.MapModel;
 
@@ -15,6 +20,8 @@ public class ServerGameManager extends GameManager {
 	private boolean randomNumbers;
 	private boolean randomPorts;
 	
+	private Map<Integer,Integer> playerIndexLookup;
+	
 	public ServerGameManager(String name, boolean randomTiles, boolean randomNumbers, boolean randomPorts, int index) 
 	{
 		super();
@@ -23,7 +30,7 @@ public class ServerGameManager extends GameManager {
 		this.randomNumbers = randomNumbers;
 		this.randomPorts = randomPorts;
 		this.randomTiles = randomTiles;
-		
+		this.playerIndexLookup = new HashMap<>();
 		this.map = MapGenerator.GenerateMap(randomTiles, randomNumbers, randomPorts);
 	}
 
@@ -33,6 +40,71 @@ public class ServerGameManager extends GameManager {
 	private void updateVersion()
 	{
 		this.version++;
+	}
+	
+	/**
+	 * Returns the player index by id
+	 * @param playerID
+	 * @return
+	 */
+	public int GetPlayerIndexByID(int playerID)
+	{
+		return playerIndexLookup.get(playerID);
+	}
+	
+	@Override
+	public int AddPlayer(String name, CatanColor color, boolean isHuman, int playerID) throws ModelException
+	{
+		int index = super.AddPlayer(name, color, isHuman, playerID);
+		playerIndexLookup.put(playerID, index);
+		
+		return index;
+	}
+	
+	/**
+	 * Sends a chat for the user
+	 * @param playerID by Player ID
+	 * @param message to chat with
+	 */
+	public boolean SendChat(int playerID, String message)
+	{
+		int playerIndex = GetPlayerIndexByID(playerID);
+		if (super.canChat(playerIndex))
+		{
+			super.PlayerChat(playerIndex, message);
+			this.updateVersion();
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param playerID
+	 * @param number
+	 * @return
+	 */
+	public boolean RollNumber(int playerID, int number)
+	{
+		int playerIndex = GetPlayerIndexByID(playerID);
+		if (!super.CanRollNumber(playerIndex)) 
+			return false;
+		//Roll the dice
+		super.DiceRoll(number);
+		this.updateVersion();
+		return true;
+	}
+	
+	/**
+	 * Rob a player
+	 * @param playerID
+	 * @param playerIndex
+	 * @param hex
+	 * @return
+	 */
+	public boolean RobPlayer(int playerID, int playerIndex, Coordinate hex)
+	{
+		return false;
 	}
 	
 	/**
